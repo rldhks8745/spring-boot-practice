6feb6f7498dbdb296df696fb775fa2cf5b774025
KimGiWan <gwkim@oneplat.co>
2021-04-15 11:45:23 +0900

    🎇Add: Exception 테스트용 추가

--- src/main/java/com/example/demo/api/user/controller/UserController.java
+++ src/main/java/com/example/demo/api/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.api.user.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,21 +17,26 @@ import com.example.demo.config.mybatis.model.PagableResponse;
 @RequestMapping(path = "/users")
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+  @Autowired
+  private UserService userService;
 
-	@GetMapping
-	public PagableResponse<User> selectUserList(@ModelAttribute UserRequest userRequest) {
-		return userService.selectUserList(userRequest);
-	}
-	
-    @GetMapping(value = "/2")
-    public List<User> selectUserList2(@ModelAttribute UserRequest userRequest) {
-        return userService.selectUserList2(userRequest);
-    }
+  @GetMapping
+  public PagableResponse<User> selectUserList(@ModelAttribute @Valid UserRequest userRequest) {
+    return userService.selectUserList(userRequest);
+  }
 
-	@GetMapping(value = "/{id}")
-	public User selectUserList(@PathVariable(name = "id") String id) {
-		return userService.selectOneUser(id);
-	}
+  @GetMapping(value = "/2")
+  public List<User> selectUserList2(@ModelAttribute UserRequest userRequest) {
+    return userService.selectUserList2(userRequest);
+  }
+
+  @GetMapping(value = "/{id}")
+  public User selectUserList(@PathVariable(name = "id") String id) {
+    return userService.selectOneUser(id);
+  }
+
+  @GetMapping(value = "/exception")
+  public User exception() throws Exception {
+    throw new Exception("exception test");
+  }
 }
