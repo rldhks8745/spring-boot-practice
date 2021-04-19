6c24c838ba30c3801520e231e088177c816b9875
KimGiWan <gwkim@oneplat.co>
2021-04-19 15:00:31 +0900

    🎃Remove: 불필요 annotation 제거

--- src/main/java/com/example/demo/config/mybatis/MybatisConfig.java
+++ src/main/java/com/example/demo/config/mybatis/MybatisConfig.java
@@ -1,14 +1,11 @@
 package com.example.demo.config.mybatis;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.example.demo.config.mybatis.interceptor.QueryInterceptor;
 import com.example.demo.config.mybatis.interceptor.StatementInterceptor;
 
 @Configuration
-@EnableConfigurationProperties(MybatisProperties.class)
 public class MybatisConfig {
   
   @Bean
