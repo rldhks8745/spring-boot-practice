0a192e7289dc0453fb90ee05f051bf96176945f0
KimGiWan <gwkim@oneplat.co>
2021-04-19 13:54:18 +0900

    🧨Modify: SqlSessionFactory 설정 -> application.yml로 변경

--- src/main/java/com/example/demo/config/mybatis/MybatisConfig.java
+++ src/main/java/com/example/demo/config/mybatis/MybatisConfig.java
@@ -1,9 +1,5 @@
 package com.example.demo.config.mybatis;
 
-import javax.sql.DataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,18 +12,12 @@ import com.example.demo.config.mybatis.interceptor.StatementInterceptor;
 public class MybatisConfig {
   
   @Bean
-  public SqlSessionFactory sqlSessionFactory(DataSource dataSource, MybatisProperties properties)
-      throws Exception {
-    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-    sqlSessionFactoryBean.setDataSource(dataSource);
-    sqlSessionFactoryBean.setConfiguration(properties.getConfiguration());
-    // sqlSessionFactoryBean.setTypeHandlersPackage("com.example.demo.config.mybatis.handler.*");
-    sqlSessionFactoryBean.setPlugins(new StatementInterceptor(), new QueryInterceptor());
-    return sqlSessionFactoryBean.getObject();
+  public StatementInterceptor statementInterceptor() {
+    return new StatementInterceptor();
   }
-
+  
   @Bean
-  public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-    return new SqlSessionTemplate(sqlSessionFactory);
+  public QueryInterceptor queryInterceptor() {
+    return new QueryInterceptor();
   }
 }
