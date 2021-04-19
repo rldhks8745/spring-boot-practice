0262e3bf7ac1564a53586cc8504797ac1ef84851
KimGiWan <gwkim@oneplat.co>
2021-04-19 14:07:49 +0900

    🎇Add: schema.sql, data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO USER (
+  id
+  ,pw
+  ,name
+) VALUES (
+  'test1'  -- id - IN varchar(50)
+  ,'test1'  -- pw - IN varchar(50)
+  ,'테스트1'  -- name - IN varchar(50)
+);
+INSERT INTO USER (
+  id
+  ,pw
+  ,name
+) VALUES (
+  'test2'  -- id - IN varchar(50)
+  ,'test2'  -- pw - IN varchar(50)
+  ,'테스트2'  -- name - IN varchar(50)
+);
+INSERT INTO USER (
+  id
+  ,pw
+  ,name
+) VALUES (
+  'test3'  -- id - IN varchar(50)
+  ,'test3'  -- pw - IN varchar(50)
+  ,'테스트3'  -- name - IN varchar(50)
+);
